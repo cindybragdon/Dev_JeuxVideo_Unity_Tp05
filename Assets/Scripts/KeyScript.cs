fba6ff11f74a4de2e26578120859016c22e625b4
cindy <cindy.bragdon@gmail.com>
2024-09-15 15:05:07 -0400

    Key is working, Doors are not

--- Assets/Scripts/KeyScript.cs
+++ Assets/Scripts/KeyScript.cs
@@ -7,7 +7,7 @@ public class KeyScript : MonoBehaviour
  
     private void OnCollisionEnter(Collision collision) {
         if (collision.collider.tag == "Player"){
-            GameObject.Find("Door").GetComponent<ExitDoor>().CanOpen=true;
+            GameObject.Find("ExitDoor").GetComponent<ExitDoor>().CanOpen=true;
             Destroy(gameObject);
             print("Canopen true");
         }
