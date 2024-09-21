fcfe156a57f97c5b2e1cbfadfb7e9d95390b8902
cindy <cindy.bragdon@gmail.com>
2024-09-21 15:38:01 -0400

    UI Canvas

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        // #if UNITY_EDITOR
+        //         UnityEditor.EditorApplication.isPlaying = false;  
+        // #else 
+        //         Application.Quit();
+        // #endif       
+    }
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene ("DemoScene");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
